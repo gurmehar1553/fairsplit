afb20925a7e894516b4878c58bf54c4848c52bfa
Jastagar <jastagar1581.be20@chitkara.edu.in>
2022-08-27 15:22:17 +0530

    new script added as gurmehar "npm run gurmehar"

--- src/code/code.js
+++ src/code/code.js
@@ -1,6 +1,3 @@
-// Online Javascript Editor for free
-// Write, Edit and Run your Javascript code using JS Online Compiler
-
 const input = [
     'A-200-A,B,C,D',
     'A-300-A,B,D',
@@ -10,8 +7,8 @@ const input = [
     'A-600-A,B,C,D',
     'A'
 ]
-const ans=[];
 function soln(newarr){
+    const ans=[];
     const tem = newarr.map((e)=>{
         return e[2]
     })
@@ -23,7 +20,7 @@ function soln(newarr){
     ans.push(firstArr)
     const a=[...NamesSet]
     const len=[...NamesSet].length
-    a.map((ele)=>{
+    a.map(ele => {
         const t=[]
         t.push(ele);
         for(let i=0;i<len;i++){
@@ -45,28 +42,5 @@ function solve(arr){
     console.log(query)
     
 }
-
-// function solve(arr){
-//     const query = arr.pop()
-//     const transformedArr = arr.map((e)=>{
-//         const [name, amount,...borrowers] = e.split('-')
-//         return [name,parseInt(amount),borrowers]
-//     })
-//     console.log(transformedArr)
-//     console.log(query)
-// }
-// function solve(arr){
-//     var ans;
-//     for(let i=0;i<arr.length;i++){
-//         var str=arr[i];
-//         var ch=str[0];
-//         var i1=str.indexOf('-');
-//         var i2=str.lastIndexOf('-');
-//         var amt=str.slice(i1,i2+1);
-//         var str2=str.slice(i2+1);
-//         var arr2=str.split(',');
-//         soln(amt,ch,arr2,ans);
-//     }
-// }
 
 solve(input)
