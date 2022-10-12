8502d73da3bbe508d9eade698b8e331035c20d7a
Jastagar <jastagar1581.be20@chitkara.edu.in>
2022-10-12 14:15:55 +0530

    bug fixed

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <div>
+        <nav className="navbar navbar-light">
+            <div className="navbar-brand ms-3 px-5 text-light">
+              <h1 className="display-6">
+                FairSplit
+              </h1>
+            </div>
+            <button className="navbar-brand btn px-5 d-flex align-items-center btn btn-success">
+                <div className="profile-icon d-inline-block me-2"></div>Your Profile
+            </button>
+        </nav>
+    </div>
+  );
+}
