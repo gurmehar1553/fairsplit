a94b9de82a31f545dd21b7cd7949d1c3cd6d7bc3
Jastagar <jastagar1581.be20@chitkara.edu.in>
2022-11-16 08:55:53 +0530

    code more structurized

--- server/controllers/loginApiHandler.js
+++ server/controllers/loginApiHandler.js
@@ -1,10 +1,7 @@
-require('dotenv').config()
-
-const loginApiHandler = require('express').Router()
-const jwt = require('jsonwebtoken')
 const Users = require('../modals/userModals')
+const loginApiHandler = require('express').Router()
 const {info}=require('../utils/logger')
-const SecretKey = process.env.SECRET_JWT_KEY
+const {authorization}=require('../utils/middleware')
 
 loginApiHandler.get("/",authorization)
 
@@ -24,38 +21,5 @@ loginApiHandler.post('/',async (req,res) => {
     }
     res.send(false)
 })
-
-function authorization(req,res){
-    const authHeader = req.get("Authorization")
-    const token = authHeader && authHeader.split(" ")[1]
-    
-    if(token == null){
-        info(false)
-        res.json({authStatus:false,user:null,})
-        return
-    }
-
-    jwt.verify(token,SecretKey, async (err, user)=>{
-
-        const userData = await Users
-        .findOne({email:user.email})
-        .populate({
-            path:'friends', 
-            populate:[
-                {path:'sentRequests', select:['username','_id']},
-                {path:'pendingRequests', select:['username','_id']},
-                {path:'currentFriends', select:['username','_id']}
-            ]
-        })
-
-        if(err){
-            info("Error is ->>>>>>>",err.message)
-            info(true)
-            res.json({authStatus:false,user:null,})
-            return
-        }
-        res.json({authStatus:true,user:userData,})
-    })
-}
 
 module.exports = loginApiHandler
