ea601f497735b6fb66a7ca5bc3b885d84d2a7f8f
Jastagar <jastagar1581.be20@chitkara.edu.in>
2022-12-04 13:41:04 +0530

    env modes added

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -1,21 +1,21 @@
 require('dotenv').config();
 
-let NodeMode = true;
+// let NodeMode = true;
 const envMode = process.env.NODE_ENV;
 
-if (envMode === 'production') {
-  console.log('Production Mode');
-  NodeMode = false;
-}
+// if (envMode === 'production') {
+//   console.log('Production Mode');
+//   NodeMode = false;
+// }
 
 function info(...args) {
-  if (NodeMode) {
+  if (!envMode === 'production') {
     console.log('-------------------------------------------------------------------');
     console.log(...args);
   }
 }
 function ShowError(...args) {
-  if (NodeMode) {
+  if (!envMode === 'production') {
     console.log('-------------------------------------------------------------------');
     console.error(...args);
   }
